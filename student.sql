165e1cf98cc6de979fe43b28e6be966b6bbe27ac
MikeSordilla <mike.sordilla@nmsc.edu.ph>
2026-03-08 18:22:34 +0800

    feat: Add SQL dump for student database with table structure and sample data

---
+++ student.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Mar 08, 2026 at 11:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_list`
+--
+
+CREATE TABLE `student_list` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `ratings` int(11) DEFAULT NULL,
+  `last_update` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_list`
+--
+
+INSERT INTO `student_list` (`id`, `firstname`, `lastname`, `ratings`, `last_update`) VALUES
+(14, 'Kuwen', 'Betonio', 100, '2026-03-08 11:19:43'),
+(18, 'Mike', 'Sordilla', 75, '2026-03-08 11:18:44');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_list`
+--
+ALTER TABLE `student_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_list`
+--
+ALTER TABLE `student_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
